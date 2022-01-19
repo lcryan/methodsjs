d6e898ad95e40de57d32a1e91a127946525fb829
lcryan <lcryan06@gmail.com>
2022-01-19 11:16:51 +0100

    DONE

--- math-methods.js
+++ math-methods.js
@@ -14,7 +14,6 @@ const circle = 4;
 console.log(Math.PI * circle);
 
 
-
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
@@ -34,6 +33,17 @@ console.log(Math.sqrt(9));
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
 // * is het willekeurige getal groter dan 0.5 ? Log dan het getal met "Het glas is halfvol" in de terminal
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
-// Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
+// Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is
+// dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+
+let result = Math.random();
+
+if (result >= 0.5) {
+    console.log(result, "Het glas is half vol");
+} else {
+    console.log(result, "Het glas is half leeg");
+}
+
 
