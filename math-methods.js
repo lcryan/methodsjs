e22192a8b337a888623e4c3a6576a678abcb3808
lcryan <lcryan06@gmail.com>
2022-01-19 10:37:13 +0100

    working on it...

--- math-methods.js
+++ math-methods.js
@@ -20,12 +20,15 @@ console.log(Math.PI * circle);
 // ---- Verwachte uitkomst: 38
 
 
+const sizeCircle = Math.PI * 12;
+
+console.log(Math.round(sizeCircle));
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
-
+console.log(Math.sqrt(9));
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -33,3 +36,4 @@ console.log(Math.PI * circle);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
